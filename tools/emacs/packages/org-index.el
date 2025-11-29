0a557dea277af798106da296c0a23b443e6354ff
Theia Ware <theia.j.ware@gmail.com>
2025-11-28 21:36:47 -0800

    feat(org-index): make index updateable
    This adds a name to the index links list, which enables
    deleting them and thus updating them too.

--- tools/emacs/packages/org-index.el
+++ tools/emacs/packages/org-index.el
@@ -6,10 +6,10 @@
 ;; Maintainer: Theia Ware
 ;; Created: November 23, 2025
 ;; Modified: November 23, 2025
-;; Version: 0.0.1
+;; Version: 0.1.0
 ;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex text tools unix vc wp
 ;; Homepage: https://github.com/theia-jane/org-index
-;; Package-Requires: ((emacs "29.1"))
+;; Package-Requires: ((emacs "30.1"))
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
@@ -42,11 +42,43 @@
 
 (defun org-index-insert ()
   (interactive)
-  (insert
-   (s-join "\n"
-           (sort
-           (nconc (org-index-other-indices default-directory)
-            (org-index-link-list default-directory))))))
+  (save-excursion
+    (goto-char (pos-bol))
+    (insert
+      "#+NAME: Index\n"
+      (s-join "\n"
+        (sort
+          (nconc (org-index-other-indices default-directory)
+            (org-index-link-list default-directory))))
+      "\n\n"))
+  (save-buffer))
+
+(defun org-index-element ()
+  (org-element-map
+      (org-element-parse-buffer)
+      'plain-list
+    '(when (string= "Index"
+                    (org-element-property :name node))
+      node)
+  nil
+  t))
+
+(defun org-index-delete ()
+  (let ((node (org-index-element)))
+  (delete-region
+    (org-element-begin node)
+    (org-element-end node))))
+
+(defun org-index-update ()
+  (interactive)
+  (when-let* ((node (org-index-element))
+              (index-loc (org-element-begin node)))
+    (save-excursion
+      (goto-char index-loc)
+      (org-index-delete)
+      (org-index-insert)))
+  (save-buffer))
+
 
 (provide 'org-index)
 ;;; org-index.el ends here
